b2b2e5adc1d7a7bc6b04fcefadada117ae14f696
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-07-08 15:14:46 +0530

    deploy

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHQaXJnT3xReaKrPmnTI5klGFsFmrGGxQ",
+  authDomain: "imdb-clone-8867e.firebaseapp.com",
+  projectId: "imdb-clone-8867e",
+  storageBucket: "imdb-clone-8867e.appspot.com",
+  messagingSenderId: "571112516395",
+  appId: "1:571112516395:web:f87481ca85306d3862b36e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
