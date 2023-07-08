f97f2439a1d97087ab52f46d209003b549a410ff
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-07-08 13:44:50 +0530

    Initial

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -4,7 +4,7 @@ import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({ result }) {
   return (
-    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group dark:border-amber-200">
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
