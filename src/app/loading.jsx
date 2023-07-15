b90afae1a4788a24d05d32cca6c5876ae0a01b9c
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-07-15 21:55:16 +0530

    deploy

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,7 @@
 export default function loading() {
     return (
       <div className="flex justify-center">
-        <img className="h-96" src="spinner.svg" alt="loading..." />
+        <img className="h-96" src="./loader.svg" alt="loading..." />
       </div>
     );
   }
